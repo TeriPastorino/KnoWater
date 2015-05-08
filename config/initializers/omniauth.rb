ae00024449ba6b3c07cf23546f83398f6735f689
Teri Pastorino <tpasto@comcast.net>
2015-05-08 09:22:38 -0700

    add tokens and key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #from intridea/omniauth
-  provider :developer unless Rails.env.production?
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  #provider :developer unless Rails.env.production?
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
   #provider :facebook, 'CONSUMER_KEY', 'CONSUMER_SECRET'
   
 end
