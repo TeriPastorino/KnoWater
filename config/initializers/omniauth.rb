e90bed4ab42e0f73d34a885280297a4d87c85c2e
Teri Pastorino <tpasto@comcast.net>
2015-05-08 12:08:56 -0700

    comment provider placeholder

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #from intridea/omniauth
   #provider :developer unless Rails.env.production?
-  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_SECRET_KEY']
   #provider :facebook, 'CONSUMER_KEY', 'CONSUMER_SECRET'
   
 end
