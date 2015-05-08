fd55a5b5a844a0d5bc954819f671fd3917e4bf20
Teri Pastorino <tpasto@comcast.net>
2015-05-08 12:07:53 -0700

    create user user/find test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".find_or_create_by_omniauth" do
+    context "when a user exists with the uid" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:auth_hash) do
+        {
+          provider: "twitter",
+          uid: user.uid,
+          info: {name: user.name }
+        }
+      end
+
+      it "finds the existing user" do
+        found_user = User.find_or_create_by_omniauth(auth_hash)
+        expect(found_user).to eq(user)
+      end
+    end
+    context "when a user does not exist with the uid" do
+      let(:auth_hash) do
+        {
+          provider: "twitter",
+          uid: "12345",
+          info: {name: "Teri" }
+        }
+      end
+
+      it "creates the new user with the appropriate attrs" do
+        user = User.find_or_create_by_omniauth(auth_hash)
+        expect(user.provider).to eq(auth_hash[:provider])
+        expect(user.uid).to eq(auth_hash[:uid])
+        expect(user.name).to eq(auth_hash[:name])
+      end
+    end
+  end
 end
