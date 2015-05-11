3e87bd0ecc36ae9797676e1490f82aedafa04cd4
Teri Pastorino <tpasto@comcast.net>
2015-05-11 14:08:37 -0700

    user.id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by_omniauth(auth_hash)
-    session[:user_id] = user_id
+    session[:user_id] = user.id
     redirect_to root_path, notice: "Sign in Successful"
   end
 
