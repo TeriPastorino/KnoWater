4611f19557c2ca8262b53a1a5c777634e4e75fa8
Teri Pastorino <tpasto@comcast.net>
2015-05-11 13:05:19 -0700

    change to uid to user_id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by_omniauth(auth_hash)
-    session[:uid] = @uid
+    session[:user_id] = @user_id
     redirect_to root_path, notice: "Sign in Successfully"
   end
 
-  
+
 
   protected
 
