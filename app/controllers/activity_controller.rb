58fb1636821ba7bb436dbc839ad862105ac5ee30
Teri Pastorino <tpasto@comcast.net>
2015-05-16 13:36:18 -0700

    add methods

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,10 +1,32 @@
 class ActivityController < ApplicationController
+  
   def index
+    @activities = Activity.all
+  end
+
+  def new
+    @activity = Activity.new(activity_params)
+    @activity.save
+  
+  def create
+    @user = current_user
+    @activity = Activity.create (activity_params[:activity])
+
+
   end
 
   def show
+    @activity = Activity.find(params[:id])
   end
 
   def update
   end
+
+  def edit
+  end
+
+  private
+    def activity_params
+      params.require(:activity)
+    end
 end
