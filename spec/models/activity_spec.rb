1c0d33f743a7f075e01736f065ff2bbdf09a9fce
Teri Pastorino <tpasto@comcast.net>
2015-05-18 22:20:07 -0700

    fix conversion number in tests

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -13,18 +13,18 @@ RSpec.describe Activity, type: :model do
   end
   describe "Callbacks" do
     describe "to ounce conversion before save" do
-      let(:activity) {FactoryGirl.create(:activity, activity_type: "shower", per_use: 2, high_efficiency: high_efficiency)}
+      let(:activity) {FactoryGirl.build(:activity, activity_type: "shower", per_use: 2, high_efficiency: high_efficiency)}
 
       context "when High Efficiency is true" do
         let(:high_efficiency) { true }
         it "converts 2 minutes in a shower to 256oz" do
-          expect {activity.save}.to change {activity.ounces}.to(256)
+          expect {activity.save}.to change {activity.ounces}.to(512)
         end
       end
       context "when High Efficiency is false" do
         let(:high_efficiency) { false }
         it "converts 2 minutes in a shower to 512oz" do
-          expect {activity.save}.to change {activity.ounces}.to(512)
+          expect {activity.save}.to change {activity.ounces}.to(1024)
         end
       end
     end
