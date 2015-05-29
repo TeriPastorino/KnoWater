ea4dbfebfc4933f2c8a54f580a5ab4bbbe87ebf7
Teri Pastorino <tpasto@comcast.net>
2015-05-28 17:20:12 -0700

    create user and activity

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
 require 'faker'
+user_attr = {"name"=>"teri l pastorino", "provider"=>"twitter", "uid"=>"460270386"}
 
+user = User.find_or_create_by({"name"=>"teri l pastorino", "provider"=>"twitter", "uid"=>"460270386"})
+  
 
-
-1.time do 
+1.times do 
   activity = Activity.create!(
-    activity_type: Shower
+    user: user,
+    activity_type: "shower",
     per_use: 7
     )
 end
